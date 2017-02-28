54d74ed37a47dbcff98fb13abebcc4ae5cd5965e
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2017-02-28 10:14:35 -0600

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "Chandubala";
+			System.out.println("Welcome to code optimization");
+			System.out.println(age);
+			
+			for(int i =0; i<age; i+=5){
+				System.out.println("Calculate BigO for this loop......");
+			}
+	}
+	
+}
