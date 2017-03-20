fbf0c913e1bd97992d5f5787be69b80590c55d83
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2017-03-20 15:53:03 -0500

    Update example1.java

--- example1.java
+++ example1.java
@@ -6,12 +6,9 @@ public class welcome(){
 			double salary = 1000.235;
 			boolean test = true;
 			String name = "Chandubala";
-			System.out.println("Welcome to code optimization");
-			System.out.println(age);
-			
-			for(int i =0; i<age; i+=5){
+			for(int i=0;i<54;i++)
+			{
 				System.out.println("Calculate BigO for this loop......");
 			}
-	}
-	
+	}	
 }
