2b4d6134ca2bfe2b80032d4b78e6b4745dfc7bf5
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2017-03-21 10:21:57 -0500

    n value extracted

--- TimeComplexity.java
+++ TimeComplexity.java
@@ -11,9 +11,8 @@ public class TimeComplexity extends inputParser {
 		Pattern p = Pattern.compile("\\s*([^\\s]*[a-zA-Z]*)\\s*[a-zA-Z]*\\s*=\\s*\\\"[a-zA-Z]*\\\"\\s*;|"
 	 		+ "\\s*([^\\s]*[a-zA-Z]*)\\s*[a-zA-Z]*\\s*=\\s*\\d+\\s*;|"
 	 		+ "\\s*([^\\s]*[a-zA-Z]*)\\s*[a-zA-Z]*\\s*=\\s*\\d+.\\d+\\s*;|\\s*([^\\s]*boolean)\\s*[a-zA-Z]*\\s*=\\s*[tT]rue\\s*;|"
-	 		+ "\\s*([^\\s]*boolean)\\s*[a-zA-Z]*\\s*=\\s*[fF]alse\\s*;"
-			+ "|\\s*while\\s*\\(.*?\\)");
-		
+	 		+ "\\s*([^\\s]*boolean)\\s*[a-zA-Z]*\\s*=\\s*[fF]alse\\s*;");
+ 	
 		Matcher m = p.matcher(wordParse);
 	 
      	
@@ -41,34 +40,51 @@ public class TimeComplexity extends inputParser {
 			 //int index = 0;
 			 System.out.println(wordParse);	
 			 int conditionalIndex1=0;
+			 
 			 conditionalIndex1 = wordParse.indexOf('<');
+			 
 			 if(conditionalIndex1 > 0){								//to make sure the value of conditionalIndex1 is not negative
-				   
-				 int secondSemiColonFinder = wordParse.indexOf(';',conditionalIndex1+1);
-				 int index = conditionalIndex1+1;
-				 String concatination = "";
-				 
-				 while(index < secondSemiColonFinder){
-					 
-					 concatination = concatination+wordParse.charAt(index);
-					 index++;
-					 
-					 //System.out.print(concatination);
-					 
-				 }
-				 System.out.println("Here is the value of n: "+concatination);
-				 
+				 extractN(conditionalIndex1,wordParse);   
+			 }
+			 else if(conditionalIndex1 < 0){
+				 conditionalIndex1 = wordParse.indexOf('>');
+				 if(conditionalIndex1 > 0)
+					 extractN(conditionalIndex1,wordParse);
 			 }
 			 else
 				 System.out.println("Couldn't find the special character");
 				 
-			 
-			
-			 //System.out.println(wordParse.charAt(index));
-				 //System.out.println(wordParse.charAt(index));
-				 
-			
 	    	}
-		}  
+		}
+ 		public static void extractN(int conditionalIndex1,String wordParse){
+ 			int secondSemiColonFinder = wordParse.indexOf(';',conditionalIndex1+1);
+			 int index = conditionalIndex1+1;
+			 String concatination = "";
+			 
+			 while(index < secondSemiColonFinder){
+				 
+				 concatination = concatination+wordParse.charAt(index);
+				 index++;
+				 
+				 //System.out.print(concatination);
+				 
+			 }
+			 System.out.println("Here is the value of n: "+concatination);
+			 char equalSymbolChecker = concatination.charAt(0);
+			 //System.out.println(equalSymbolChecker);
+			 if(equalSymbolChecker == '='){
+				 String trimmedString = concatination.substring(1); //to remove = in the extracted string
+				 //System.out.println(trimmedString);
+				 int nValue = Integer.parseInt(trimmedString); //to convert extracted string to integer
+				 // System.out.println(nValue+20); //cross check				 
+				 
+			 }
+			 else{
+				 int nValue = Integer.parseInt(concatination);
+				 System.out.println(nValue+20); //cross check
+				 
+			 }
+			
+ 		}
 	}
 
