d7c7fcebf3e100f5999c5ef24c4e9be1fa1991cc
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2017-03-22 22:29:57 -0500

    Update inputParser.java

--- inputParser.java
+++ inputParser.java
@@ -5,18 +5,18 @@ import java.util.*;
 public class inputParser {
 	String line = "NULL";
 	
-	public void lineParser() throws FileNotFoundException {	
+	public void lineParser() throws FileNotFoundException, StackUnderflowException {	
 		TimeComplexity tc  = new TimeComplexity();
 		Scanner input = new Scanner(new File("example1.java")); // input java file
 		/*Line by line parsing*/
-		System.out.println(System.currentTimeMillis());
 		while (input.hasNextLine()) {
         line = input.nextLine();
         TimeComplexity.processLineInit(line);//call the method for each and every line of input
-        TimeComplexity.processLineForLoop(line);
+        TimeComplexity.processLineForLoop(line);//calls this method to search for for loop in the input
+        
                
     }
-	System.out.println(System.currentTimeMillis());
+	
 	//tc.findKeyWord(line);
     input.close();
 }  
