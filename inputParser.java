221cd63fdb85071ca3283cb0b77d30e3c549ba5c
Chandra sekhar Polavarapu <chandra.polavarapu516@topper.wku.edu>
2017-04-07 22:59:44 -0500

    with a line counter

--- inputParser.java
+++ inputParser.java
@@ -5,14 +5,18 @@ import java.util.*;
 public class inputParser {
 	String line = "NULL";
 	
-	public void lineParser() throws FileNotFoundException, StackUnderflowException {	
+	public void lineParser() throws FileNotFoundException, StackUnderflowException {
+		int lineCounter =0;
 		TimeComplexity tc  = new TimeComplexity();
+		SpaceComplexity sc = new SpaceComplexity();
 		Scanner input = new Scanner(new File("example1.java")); // input java file
 		/*Line by line parsing*/
 		while (input.hasNextLine()) {
-        line = input.nextLine();
-        TimeComplexity.processLineInit(line);//call the method for each and every line of input
-        TimeComplexity.processLineForLoop(line);//calls this method to search for for loop in the input
+			line = input.nextLine();
+			lineCounter++;
+			TimeComplexity.processLineInit(line,lineCounter);//call the method for each and every line of input
+			TimeComplexity.processLineForLoop(line,lineCounter);//calls this method to search for for loop in the input
+			sc.processLineInit(line);
         
                
     }
