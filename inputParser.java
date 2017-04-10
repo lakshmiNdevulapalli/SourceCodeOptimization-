4f5c790044f165762b162c68775f4a7770a8be7f
Chandra sekhar Polavarapu <chandra.polavarapu516@topper.wku.edu>
2017-04-10 15:53:18 -0500

    Input updated

--- inputParser.java
+++ inputParser.java
@@ -9,19 +9,26 @@ public class inputParser {
 		int lineCounter =0;
 		TimeComplexity tc  = new TimeComplexity();
 		SpaceComplexity sc = new SpaceComplexity();
+		int timeComplexity = 0;
 		Scanner input = new Scanner(new File("example1.java")); // input java file
 		/*Line by line parsing*/
 		while (input.hasNextLine()) {
 			line = input.nextLine();
 			lineCounter++;
 			TimeComplexity.processLineInit(line,lineCounter);//call the method for each and every line of input
+			//System.out.println("Testing at input parser"+tc.initComplexity);
+			
 			TimeComplexity.processLineForLoop(line,lineCounter);//calls this method to search for for loop in the input
-			sc.processLineInit(line);
+			
+			
+			
+			//sc.processLineInit(line);
         
                
     }
-	
-	//tc.findKeyWord(line);
+		timeComplexity = tc.initComplexity+tc.forLoopComplexity;
+		System.out.println("total time complexity is: "+timeComplexity);
+	//tc.findKeyWord(line)
     input.close();
 }  
 
