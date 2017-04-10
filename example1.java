eeba729904c631f0614d3d739713dd816addbe28
Chandra sekhar Polavarapu <chandra.polavarapu516@topper.wku.edu>
2017-04-10 15:54:16 -0500

    updated input file

--- example1.java
+++ example1.java
@@ -5,10 +5,15 @@ public class welcome(){
 			float gpa = 3.33;
 			double salary = 1000.235;
 			boolean test = true;
-			String name = "Chandubala";
-			for(int i=0;i<54;i++)
+			String name = "Chandu bala";
+			for(int i=0;i>50;i++)
 			{
 				System.out.println("Calculate BigO for this loop......");
-			}
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = age*salary;
+				System.out.println(mathExpression);
+		}
+			int age = 22;
 	}	
 }
