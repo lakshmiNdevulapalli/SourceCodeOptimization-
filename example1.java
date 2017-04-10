6242f982f71840601edddee3d83ca463112372d6
Chandra sekhar Polavarapu <chandra.polavarapu516@topper.wku.edu>
2017-04-10 18:17:12 -0500

    updated input file

--- example1.java
+++ example1.java
@@ -5,15 +5,23 @@ public class welcome(){
 			float gpa = 3.33;
 			double salary = 1000.235;
 			boolean test = true;
-			String name = "Chandu bala";
+			String name = "initial string";
 			for(int i=0;i>50;i++)
 			{
 				System.out.println("Calculate BigO for this loop......");
 				int a = 50;
 				String check = "Am the string inside the forloop";
-				double mathExpression = age*salary;
+				double mathExpression = 5*5;
 				System.out.println(mathExpression);
-		}
+			}
 			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int bala = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
 	}	
 }
