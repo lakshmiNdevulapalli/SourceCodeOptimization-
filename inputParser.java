a09c0c52d172c7b0c543c4a9020a5cf83d28dba5
Chandra sekhar Polavarapu <chandra.polavarapu516@topper.wku.edu>
2017-04-10 19:32:03 -0500

    includes If

--- inputParser.java
+++ inputParser.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class inputParser {
 	String line = "NULL";
 	
+	@SuppressWarnings("static-access")
 	public void lineParser() throws FileNotFoundException, StackUnderflowException {
 		int lineCounter =0;
 		TimeComplexity tc  = new TimeComplexity();
@@ -16,15 +17,16 @@ public class inputParser {
 		while (input.hasNextLine()) {
 			line = input.nextLine();
 			lineCounter++;
-			tc.processLineInit(line,lineCounter);//call the method for each and every line of input
+			tc.processLineInit(line,lineCounter);//calls this method to recognize initial statements and import statements
 			tc.processLineForLoop(line,lineCounter);//calls this method to search for for loop in the input
-			tc.processWhileLoop(line, lineCounter);
-			tc.processPrints(line);
-			sc.processLineInit(line);
+			tc.processWhileLoop(line, lineCounter);//calls this method to search for while loop in the input
+			tc.processPrints(line);//calls this method to recognize print statements in input
+			tc.processIf(line, lineCounter);
+			//sc.processLineInit(line);
 		}
 		System.out.println("===================================================\n Total time complexity of print statments : "+tc.printComplexity+"\n===============================================");
 
-		timeComplexity = tc.initComplexity+tc.forLoopComplexity+tc.whileComplexity+tc.printComplexity;
+		timeComplexity = tc.initComplexity+tc.forLoopComplexity+tc.whileComplexity+tc.printComplexity+tc.ifComplexity;
 		System.out.println("total time complexity is: "+timeComplexity);
 		/*Space complexity loop*/
 		
