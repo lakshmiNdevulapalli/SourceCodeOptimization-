893b3498ffe0025128ad310c1f54d9b1fa451c42
Chandra sekhar Polavarapu <chandra.polavarapu516@topper.wku.edu>
2017-04-29 17:33:40 -0500

    Update mainMethod.java

--- mainMethod.java
+++ mainMethod.java
@@ -1,12 +1,19 @@
 import java.io.*;
+import java.lang.management.ManagementFactory;
+import java.nio.file.FileStore;
 import java.util.Scanner;
 
 public class mainMethod {
-    public static void main(String[] args)throws FileNotFoundException {
-            inputParser ip = new inputParser();
+    public static void main(String[] args)throws FileNotFoundException, StackUnderflowException {
+    		/*Object creation*/
+    		
+    		long beforeUsedMem = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
+            inputParser ip = new inputParser();	
             TimeComplexity tc  = new TimeComplexity();
-           ip.lineParser();
+            ip.lineParser();
+            long AfterUsedMem = Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
            
+           System.out.println("Total memory used for this program : "+(AfterUsedMem-beforeUsedMem)+" bytes");
     }
 } 
     	
