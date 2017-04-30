d0c5cbed8dc6df01595de21629100987b820c88c
lakshmiNdevulapalli <baludevulapalli@gmail.com>
2017-04-29 21:23:39 -0500

    unit test input file

---
+++ input.java
@@ -0,0 +1,272 @@
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
+import java.io.*;
+public class welcome(){
+	public static void main(String[] args){
+			int age = 22;
+			float gpa = 3.33;
+			double salary = 1000.235;
+			boolean test = true;
+			String name = "initial string";
+			int nnumberArray[5]={1,2,3,4,5};
+			for(int i=0;i>50;i++)
+			{
+				System.out.println("Calculate BigO for this loop......");
+				int a = 50;
+				String check = "Am the string inside the forloop";
+				double mathExpression = 5*5;
+				System.out.println(mathExpression);
+			}
+			int age = 22;
+			while(age<=10)
+			{
+				System.out.println("Am inside while loop");
+				String whiler = "String in while loop";
+				int number = 99;
+			}
+			System.out.println("checking print statments");
+			System.out.print("Checking print statements 2");
+			if(a<b)
+			{
+				System.out.println("Am inside if condition");
+				int number = 100;
+				a = b*a;				
+			}
+	}	
+}
